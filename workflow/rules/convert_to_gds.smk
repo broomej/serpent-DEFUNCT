4acf91234f16124f434448e1a0b6d379f3f30f0d
Jai Broome <jb@jaibroome.com>
2025-10-01 14:15:46 -0700

    add arguments and parameters for plink_to_gds

--- workflow/rules/convert_to_gds.smk
+++ workflow/rules/convert_to_gds.smk
@@ -3,4 +3,5 @@ rule plink_to_gds:
         plink_prefix=config["plink_prefix"]
     output:
         gds="results/gds.gds"
+    params: lookup(within=config, dpath="plink_to_gds"),
     script: "scripts/plink_to_gds.R"
--- workflow/scripts/plink_to_gds.R
+++ workflow/scripts/plink_to_gds.R
@@ -1,10 +1,7 @@
 #! /usr/bin/env Rscript
 input <- snakemake@input
-output <- snakemake@output
-SeqArray::seqBED2GDS(
-    bed.fn = input$bed_file,
-    fam.fn = input$fam_file,
-    bim.fn = input$bim_file,
-    out.gdsfn=output$gds
-)
+input[c("bed_file", "fam_file", "bim_file")] <- paste0(input$plink_prefix, c(".bed", ".fam", ".bim"))
+input <- input[!(names(input) %in% "plink_prefix")]
+arguments <- c(input, snakemake@params, snakemake@output)
+do.call(SeqArray::seqBED2GDS, arguments)
 print(sessionInfo())
